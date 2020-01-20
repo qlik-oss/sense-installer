19ed3d5e60885c0f729b218935e985f4740bd439
Ashwathi Shiva <ashwathi.shiva@qlik.com>
2020-01-20 16:31:48 -0500

    removed duplicate constant again

--- cmd/qliksense/root.go
+++ cmd/qliksense/root.go
@@ -21,7 +21,6 @@ import (
 )
 
 const (
-	// porterURLBase    = "https://cdn.deislabs.io/porter/"
 	winOS            = "porter-windows-amd64.exe"
 	linuxOS          = "porter-linux-amd64"
 	macOS            = "porter-darwin-amd64"
