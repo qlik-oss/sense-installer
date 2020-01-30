e411219da82ec03333103c67e7008da8b53686cd
Foysal Iqbal <mqb@qlik.com>
2020-01-30 15:37:17 -0500

    bring static files into cli (#49)

--- pkg/qliksense/qliksense.go
+++ pkg/qliksense/qliksense.go
@@ -1,9 +1,15 @@
+//go:generate packr2
 package qliksense
+
+import (
+	"github.com/gobuffalo/packr/v2"
+)
 
 // Qliksense is the logic behind the qliksense client
 type Qliksense struct {
 	PorterExe     string
 	QliksenseHome string
+	CrdBox        *packr.Box
 }
 
 // New qliksense client, initialized with useful defaults.
@@ -11,5 +17,6 @@ func New(porterExe, qliksenseHome string) *Qliksense {
 	return &Qliksense{
 		PorterExe:     porterExe,
 		QliksenseHome: qliksenseHome,
+		CrdBox:        packr.New("crds", "./crds"),
 	}
 }
