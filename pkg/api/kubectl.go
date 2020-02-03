30b74eca66f76e4ec9a72212484b38dd2918436c
Foysal Iqbal <mqb@qlik.com>
2020-02-03 10:10:03 -0500

    add kubectl apply

---
+++ pkg/api/kubectl.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+)
+
+func KubectlApply(manifests string) error {
+	tempYaml, err := ioutil.TempFile("", "")
+	if err != nil {
+		fmt.Println("cannot create file ", err)
+		return err
+	}
+	tempYaml.WriteString(manifests)
+
+	cmd := exec.Command("kubectl", "apply", "-f", tempYaml.Name())
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		fmt.Printf("kubectl apply failed with %s\n", err)
+		return err
+	}
+	return nil
+}
