68db3f965a549fcfb75b66037b64263cc6e44f11
Ashwathi Shiva <ashwathi.shiva@qlik.com>
2020-02-07 12:38:32 -0500

    fixed an accidental merge conflict commit

--- pkg/qliksense/context_configs.go
+++ pkg/qliksense/context_configs.go
@@ -274,7 +274,6 @@ func SetUpQliksenseDefaultContext(qlikSenseHome string) {
 
 // SetUpQliksenseContext - to setup qliksense context
 func SetUpQliksenseContext(qlikSenseHome, contextName string, isDefaultContext bool) {
-<<<<<<< HEAD
 	// check the length of the context name entered by the user, it should not exceed 17 chars
 	if len(contextName) > MaxContextNameLength {
 		log.Fatal("Please enter a context-name with utmost 17 characters")
@@ -284,11 +283,6 @@ func SetUpQliksenseContext(qlikSenseHome, contextName string, isDefaultContext b
 	var qliksenseConfig api.QliksenseConfig
 	configFileTrack := false
 
-=======
-	qliksenseConfigFile := filepath.Join(qlikSenseHome, QliksenseConfigFile)
-	var qliksenseConfig api.QliksenseConfig
-	configFileTrack := false
->>>>>>> ms-3
 	if !FileExists(qliksenseConfigFile) {
 		qliksenseConfig = AddBaseQliksenseConfigs(qliksenseConfig, contextName)
 	} else {
