a29e7acf704a5376a1b310288ad4185ec635beab
Sanat Nayar <fqg@qlik.com>
2020-02-12 17:03:34 -0500

    init

---
+++ cmd/qliksense/upgrade.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/qlik-oss/sense-installer/pkg/qliksense"
+	"github.com/spf13/cobra"
+)
+
+func upgradeCmd(q *qliksense.Qliksense) *cobra.Command {
+	opts := &qliksense.InstallCommandOptions{}
+	c := &cobra.Command{
+		Use:     "upgrade",
+		Short:   "upgrade qliksense release",
+		Long:    `upgrade qliksesne release`,
+		Example: `qliksense upgrade <version>`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return q.UpgradeQK8s(opts)
+		},
+	}
+
+	return c
+}
---
+++ pkg/qliksense/upgrade.go
@@ -0,0 +1,47 @@
+package qliksense
+
+import (
+	"fmt"
+
+	qapi "github.com/qlik-oss/sense-installer/pkg/api"
+)
+
+type upgradeCommandOptions struct {
+	AcceptEULA   string
+	Namespace    string
+	StorageClass string
+	MongoDbUri   string
+	RotateKeys   string
+}
+
+func (q *Qliksense) UpgradeQK8s(opts *InstallCommandOptions) error {
+
+	// step1: get CR
+	// step2: run kustomize
+	// step3: run kubectl apply
+
+	// fetch the version
+	qConfig := qapi.NewQConfig(q.QliksenseHome)
+	fmt.Println(qConfig)
+
+	qcr, err := qConfig.GetCurrentCR()
+	if err != nil {
+		fmt.Println("cannot get the current-context cr", err)
+		return err
+	}
+	qConfig.WriteCurrentContextCR(qcr)
+	if err := q.applyConfigToK8s(qcr); err != nil {
+		fmt.Println("cannot do kubectl apply on manifests")
+		return err
+	}
+	fmt.Println("Install operator CR into cluster")
+	r, err := q.getCurrentCRString()
+	if err != nil {
+		return err
+	}
+	if err := qapi.KubectlApply(r); err != nil {
+		fmt.Println("cannot do kubectl apply on operator CR")
+	}
+	return nil
+
+}
