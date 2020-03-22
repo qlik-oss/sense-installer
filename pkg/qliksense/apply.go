4e533c5999113de4f6e96a766917c8dda95c562f
Foysal Iqbal <mqb@qlik.com>
2020-03-21 23:57:23 -0400

    add doc for one liner
    Signed-off-by: Foysal Iqbal <mqb@qlik.com>

--- pkg/qliksense/apply.go
+++ pkg/qliksense/apply.go
@@ -2,14 +2,21 @@ package qliksense
 
 import (
 	"io"
+
+	qapi "github.com/qlik-oss/sense-installer/pkg/api"
 )
 
 func (q *Qliksense) ApplyCRFromReader(r io.Reader) error {
 	if err := q.LoadCr(r); err != nil {
 		return err
 	}
+	qConfig := qapi.NewQConfig(q.QliksenseHome)
+	cr, err := qConfig.GetCurrentCR()
+	if err != nil {
+		return err
+	}
 	opts := &InstallCommandOptions{}
-	if err := q.InstallQK8s("", opts, true); err != nil {
+	if err := q.InstallQK8s(cr.GetLabelFromCr("version"), opts, true); err != nil {
 		return err
 	}
 	return nil
