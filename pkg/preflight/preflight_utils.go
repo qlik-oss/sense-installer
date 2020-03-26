1dd88195c9ee4650d903b5e9e955e6e40dce9375
Ashwathi Shiva <ashwathi.shiva@qlik.com>
2020-03-25 22:44:12 -0400

    Windows yaml file path modified to address error- unsupported protocol schema

--- pkg/preflight/preflight_utils.go
+++ pkg/preflight/preflight_utils.go
@@ -145,7 +145,14 @@ func initiateK8sOps(opr, namespace string) error {
 
 func invokePreflight(preflightCommand string, yamlFile *os.File) error {
 	arguments := []string{}
-	arguments = append(arguments, yamlFile.Name(), "--interactive=false")
+	// check for 2nd character is ':' then take file location from after ':'
+	tempYamlName := yamlFile.Name()
+	if tempYamlName[1] == ':' {
+		tempYamlName = tempYamlName[2:]
+		api.LogDebugMessage("This is the Windows yaml file path after modification: %s", tempYamlName)
+	}
+
+	arguments = append(arguments, tempYamlName, "--interactive=false")
 	cmd := exec.Command(preflightCommand, arguments...)
 
 	sterrBuffer := &bytes.Buffer{}
