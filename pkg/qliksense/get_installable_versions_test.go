9143a54e8ca531f7b87871ebab2d97656a930736
Foysal Iqbal <mqb@qlik.com>
2020-04-15 18:34:40 -0400

    add semver test
    Signed-off-by: Foysal Iqbal <mqb@qlik.com>

--- pkg/qliksense/get_installable_versions_test.go
+++ pkg/qliksense/get_installable_versions_test.go
@@ -2,12 +2,24 @@ package qliksense
 
 import (
 	"testing"
+
+	"github.com/Masterminds/semver/v3"
 )
 
 func TestGetLatestTag(t *testing.T) {
 	s, err := getLatestTag(defaultConfigRepoGitUrl, "")
 	if s == "" || err != nil {
 		t.Log(err)
+		t.Fail()
+	}
+	sv, err := semver.NewVersion(s)
+	if err != nil {
+		t.Log(err)
+		t.Log(sv)
+	}
+	baseV, _ := semver.NewVersion("v0.0.7")
+	if !sv.GreaterThan(baseV) {
+		t.Log("Expected greater than v0.0.7, but got:  " + s)
 		t.Fail()
 	}
 }
