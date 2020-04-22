cc73626d75f57c41e56d2cbbde8b4161976a74aa
Andriy Bulynko <andriy.bulynko@qlik.com>
2020-04-22 16:24:06 -0400

    Adding a concurrency test for a version of qlik-oss config

--- pkg/qliksense/kuz_test.go
+++ pkg/qliksense/kuz_test.go
@@ -3,13 +3,24 @@ package qliksense
 import (
 	"bytes"
 	"encoding/base64"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
-	"path"
+	"path/filepath"
+	"regexp"
 	"strings"
+	"sync"
+	"sync/atomic"
 	"testing"
+	"time"
+
+	"sigs.k8s.io/kustomize/api/k8sdeps/kunstruct"
+	"sigs.k8s.io/kustomize/api/resid"
+	"sigs.k8s.io/kustomize/api/resmap"
+	"sigs.k8s.io/kustomize/api/resource"
+	"sigs.k8s.io/kustomize/api/types"
 
 	"gopkg.in/yaml.v3"
 
@@ -27,7 +38,7 @@ func Test_ExecuteKustomizeBuild(t *testing.T) {
 	}
 	defer os.RemoveAll(tmpDir)
 
-	kustomizationYamlFilePath := path.Join(tmpDir, "kustomization.yaml")
+	kustomizationYamlFilePath := filepath.Join(tmpDir, "kustomization.yaml")
 	kustomizationYaml := `
 generatorOptions:
   disableNameSuffixHash: true
@@ -58,6 +69,203 @@ metadata:
 	}
 }
 
+func Test_executeKustomizeBuild_onQlikConfig_withConcurrency(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping parallel kustomize test in short mode")
+	}
+
+	tmpDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatalf("unexpected error: %v\n", err)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	configPath := filepath.Join(tmpDir, "config")
+	if repo, err := kapis_git.CloneRepository(configPath, defaultConfigRepoGitUrl, nil); err != nil {
+		t.Fatalf("unexpected error: %v\n", err)
+	} else if err := kapis_git.Checkout(repo, "v0.0.8", "v0.0.8", nil); err != nil {
+		t.Fatalf("unexpected error: %v\n", err)
+	}
+
+	configsDir := filepath.Join(configPath, "manifests", "base", "manifests")
+
+	shouldSkipDir := func(dirName string) bool {
+		skipMap := map[string]bool{
+			"sense-client": true,
+		}
+		_, ok := skipMap[dirName]
+		return ok
+	}
+
+	insertConcatenationKuzYaml := func(dir string) error {
+		concatenatedMap := map[string]interface{}{}
+		concatenatedMap["apiVersion"] = "kustomize.config.k8s.io/v1beta1"
+		concatenatedMap["kind"] = "Kustomization"
+		resources := make([]string, 0)
+		infos, err := ioutil.ReadDir(dir)
+		if err != nil {
+			return err
+		}
+		for _, info := range infos {
+			if !info.IsDir() {
+				resources = append(resources, info.Name())
+			}
+		}
+		concatenatedMap["resources"] = resources
+
+		if concatenatedMapBytes, err := yaml.Marshal(&concatenatedMap); err != nil {
+			return err
+		} else if err := ioutil.WriteFile(filepath.Join(dir, "kustomization.yaml"), concatenatedMapBytes, os.ModePerm); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	//sequential test:
+	sequentialManifestsDir := filepath.Join(tmpDir, "sequential")
+	if err := os.RemoveAll(sequentialManifestsDir); err != nil {
+		t.Fatalf("unexpected error: %v\n", err)
+	} else if err := os.MkdirAll(sequentialManifestsDir, os.ModePerm); err != nil {
+		t.Fatalf("unexpected error: %v\n", err)
+	} else if err := os.RemoveAll(filepath.Join(os.TempDir(), "dotHelm")); err != nil {
+		t.Fatalf("unexpected error: %v\n", err)
+	} else if err := os.RemoveAll(filepath.Join(os.TempDir(), ".chartcache")); err != nil {
+		t.Fatalf("unexpected error: %v\n", err)
+	}
+
+	infos, err := ioutil.ReadDir(configsDir)
+	if err != nil {
+		t.Fatalf("unexpected error: %v\n", err)
+	}
+	fmt.Print("running a sequential execution test...\n")
+	t1 := time.Now()
+	for _, info := range infos {
+		if shouldSkipDir(info.Name()) {
+			continue
+		}
+		if info.IsDir() {
+			if yamlResources, err := ExecuteKustomizeBuild(filepath.Join(configsDir, info.Name())); err != nil {
+				t.Fatalf("unexpected error kustomizing: %v, error: %v\n", info.Name(), err)
+			} else if err := ioutil.WriteFile(filepath.Join(sequentialManifestsDir, fmt.Sprintf("%v.yaml", info.Name())), yamlResources, os.ModePerm); err != nil {
+				t.Fatalf("unexpected error: %v\n", err)
+			}
+		}
+	}
+	t2 := time.Now()
+	fmt.Printf("sequential execution test took: %vs\n", t2.Sub(t1).Seconds())
+	if err := insertConcatenationKuzYaml(sequentialManifestsDir); err != nil {
+		t.Fatalf("unexpected error: %v\n", err)
+	}
+	concatenatedSequentialManifests, err := ExecuteKustomizeBuild(sequentialManifestsDir)
+	if err != nil {
+		t.Fatalf("unexpected error: %v\n", err)
+	}
+
+	//concurrent tests:
+	numConcurrentTests := 5
+	concatenatedConcurrentManifestsList := make([][]byte, 0)
+	for i := 0; i < numConcurrentTests; i++ {
+		concurrentManifestsDir := filepath.Join(tmpDir, fmt.Sprintf("concurrent-%v", i))
+		if err := os.RemoveAll(concurrentManifestsDir); err != nil {
+			t.Fatalf("unexpected error: %v\n", err)
+		} else if err := os.MkdirAll(concurrentManifestsDir, os.ModePerm); err != nil {
+			t.Fatalf("unexpected error: %v\n", err)
+		} else if err := os.RemoveAll(filepath.Join(os.TempDir(), "dotHelm")); err != nil {
+			t.Fatalf("unexpected error: %v\n", err)
+		} else if err := os.RemoveAll(filepath.Join(os.TempDir(), ".chartcache")); err != nil {
+			t.Fatalf("unexpected error: %v\n", err)
+		}
+
+		var wg sync.WaitGroup
+		var concurrentErrorCounter int32
+		osStderrBackup := os.Stderr
+		tmpStdErrFile, err := ioutil.TempFile("", "")
+		if err != nil {
+			t.Fatalf("unexpected error: %v\n", err)
+		}
+		os.Stderr = tmpStdErrFile
+		fmt.Print("running a concurrent execution test...\n")
+		t1 = time.Now()
+		for _, fi := range infos {
+			wg.Add(1)
+			go func(info os.FileInfo) {
+				defer wg.Done()
+				if shouldSkipDir(info.Name()) {
+					return
+				}
+				if info.IsDir() {
+					if yamlResources, err := ExecuteKustomizeBuild(filepath.Join(configsDir, info.Name())); err != nil {
+						fmt.Printf("unexpected error: %v\n", err)
+						atomic.AddInt32(&concurrentErrorCounter, 1)
+					} else if err := ioutil.WriteFile(filepath.Join(concurrentManifestsDir, fmt.Sprintf("%v.yaml", info.Name())), yamlResources, os.ModePerm); err != nil {
+						fmt.Printf("unexpected error: %v\n", err)
+						atomic.AddInt32(&concurrentErrorCounter, 1)
+					}
+				}
+			}(fi)
+		}
+		wg.Wait()
+		t2 = time.Now()
+		os.Stderr = osStderrBackup
+		os.Remove(tmpStdErrFile.Name())
+		if concurrentErrorCounter > 0 {
+			t.Fatalf("there were %v errors during the concurrent execution", concurrentErrorCounter)
+		}
+		fmt.Printf("concurrent execution test took: %vs\n", t2.Sub(t1).Seconds())
+		if err := insertConcatenationKuzYaml(concurrentManifestsDir); err != nil {
+			t.Fatalf("unexpected error: %v\n", err)
+		}
+		concatenatedConcurrentManifests, err := ExecuteKustomizeBuild(concurrentManifestsDir)
+		if err != nil {
+			t.Fatalf("unexpected error: %v\n", err)
+		}
+		concatenatedConcurrentManifestsList = append(concatenatedConcurrentManifestsList, concatenatedConcurrentManifests)
+	}
+
+	getResMapBytesAdjustedForCaCertsJobName := func(resBytes []byte) ([]byte, error) {
+		resMapFactory := resmap.NewFactory(resource.NewFactory(kunstruct.NewKunstructuredFactoryImpl()), nil)
+		if resMap, err := resMapFactory.NewResMapFromBytes(resBytes); err != nil {
+			return nil, err
+		} else if resources, err := resMap.Select(types.Selector{
+			Gvk: resid.Gvk{
+				Group:   "batch",
+				Version: "v1",
+				Kind:    "Job",
+			},
+		}); err != nil {
+			return nil, err
+		} else if re, err := regexp.Compile(`^.+-ca-certificates-[a-z]{5}$`); err != nil {
+			return nil, err
+		} else {
+			for _, res := range resources {
+				res.SetName(re.ReplaceAllString(res.GetName(), "qliksense-ca-certificates"))
+			}
+			return resMap.AsYaml()
+		}
+	}
+
+	sequentialFinalManifest, err := getResMapBytesAdjustedForCaCertsJobName(concatenatedSequentialManifests)
+	if err != nil {
+		t.Fatalf("unexpected error: %v\n", err)
+	}
+
+	concurrentFinalManifestsList := make([][]byte, 0)
+	for _, concatenatedConcurrentManifest := range concatenatedConcurrentManifestsList {
+		if concurrentFinalManifest, err := getResMapBytesAdjustedForCaCertsJobName(concatenatedConcurrentManifest); err != nil {
+			t.Fatalf("unexpected error: %v\n", err)
+		} else {
+			concurrentFinalManifestsList = append(concurrentFinalManifestsList, concurrentFinalManifest)
+		}
+	}
+
+	for _, concurrentFinalManifest := range concurrentFinalManifestsList {
+		if !bytes.Equal(concurrentFinalManifest, sequentialFinalManifest) {
+			t.Fatalf("expected the concatenated concurrent manifest to equal the concatenated sequential manifest, but they didn't..."+
+				"\nconcurrent:\n%v\nsequential:\n%v", string(concurrentFinalManifest), string(sequentialFinalManifest))
+		}
+	}
+}
+
 func Test_executeKustomizeBuild_onQlikConfig_regenerateKeys(t *testing.T) {
 	tmpDir, err := ioutil.TempDir("", "")
 	if err != nil {
@@ -65,7 +273,7 @@ func Test_executeKustomizeBuild_onQlikConfig_regenerateKeys(t *testing.T) {
 	}
 	defer os.RemoveAll(tmpDir)
 
-	configPath := path.Join(tmpDir, "config")
+	configPath := filepath.Join(tmpDir, "config")
 	if repo, err := kapis_git.CloneRepository(configPath, defaultConfigRepoGitUrl, nil); err != nil {
 		t.Fatalf("unexpected error: %v\n", err)
 	} else if err := kapis_git.Checkout(repo, "e38df644e759abf0b5941c1511d1a2cd5e3c42fa", "", nil); err != nil {
@@ -86,7 +294,7 @@ func Test_executeKustomizeBuild_onQlikConfig_regenerateKeys(t *testing.T) {
 
 	generateKeys(cr, "won't-use")
 
-	yamlResources, err := ExecuteKustomizeBuild(path.Join(configPath, "manifests", "base", "resources", "users"))
+	yamlResources, err := ExecuteKustomizeBuild(filepath.Join(configPath, "manifests", "base", "resources", "users"))
 	if err != nil {
 		t.Fatalf("unexpected kustomize error: %v\n", err)
 	}
@@ -127,7 +335,7 @@ func generateKeys(cr *config.CRSpec, defaultKeyDir string) {
 		log.Printf("error generating application keys: %v\n", err)
 	} else if err := os.MkdirAll(keyDir, os.ModePerm); err != nil {
 		log.Printf("error makeing sure private key storage directory: %v exists, error: %v\n", keyDir, err)
-	} else if err := ioutil.WriteFile(path.Join(keyDir, ejsonPublicKey), []byte(ejsonPrivateKey), os.ModePerm); err != nil {
+	} else if err := ioutil.WriteFile(filepath.Join(keyDir, ejsonPublicKey), []byte(ejsonPrivateKey), os.ModePerm); err != nil {
 		log.Printf("error storing ejson private key: %v\n", err)
 	}
 }
@@ -147,8 +355,8 @@ func Test_GetYamlDocKindFromMultiDoc(t *testing.T) {
 	}
 	defer os.RemoveAll(tmpDir)
 
-	kustomizationYamlFilePath := path.Join(tmpDir, "kustomization.yaml")
-	testResFileYamlFilePath := path.Join(tmpDir, "test-file.yaml")
+	kustomizationYamlFilePath := filepath.Join(tmpDir, "kustomization.yaml")
+	testResFileYamlFilePath := filepath.Join(tmpDir, "test-file.yaml")
 	kustomizationYaml := `
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
