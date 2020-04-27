02f7b2006ab0c1c2eb7b853f673e40029a09ebf4
Sanat Nayar <sanatnayar@gmail.com>
2020-04-27 09:58:42 -0400

    init

--- cmd/qliksense/root.go
+++ cmd/qliksense/root.go
@@ -202,6 +202,9 @@ func rootCmd(p *qliksense.Qliksense) *cobra.Command {
 	// add uninstall command
 	cmd.AddCommand(uninstallCmd(p))
 
+	// add export command
+	cmd.AddCommand(exportCmd(p))
+
 	// add crds
 	cmd.AddCommand(crdsCmd)
 	crdsCmd.AddCommand(crdsViewCmd(p))
