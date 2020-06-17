7cc6e55779fb7572c569a11d2921cf4b9373bb85
Foysal Iqbal <mqb@qlik.com>
2020-06-17 09:16:53 -0400

    remove fetch source
    Signed-off-by: Foysal Iqbal <mqb@qlik.com>

--- pkg/api/copy_test.go
+++ pkg/api/copy_test.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"reflect"
 	"testing"
 
 	kapis_git "github.com/qlik-oss/k-apis/pkg/git"
@@ -28,6 +29,7 @@ func TestCopyDirectory(t *testing.T) {
 }
 
 func TestCopyDirectory_withGit_withKuz(t *testing.T) {
+	ver := "master"
 	if testing.Short() {
 		t.Skip("Skipping in short test mode")
 	}
@@ -45,43 +47,24 @@ func TestCopyDirectory_withGit_withKuz(t *testing.T) {
 	defer os.RemoveAll(tmpDir2)
 
 	repoPath1 := path.Join(tmpDir1, "repo")
-	repo1, err := kapis_git.CloneRepository(repoPath1, "https://github.com/qlik-oss/qliksense-k8s", nil)
+	_, err = kapis_git.CloneRepository(repoPath1, "https://github.com/qlik-oss/qliksense-k8s", nil)
 	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	repo1, err := kapis_git.OpenRepository(repoPath1)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if err := kapis_git.Checkout(repo1, ver, "", nil); err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
 
 	if err := CopyDirectory(repoPath1, tmpDir2); err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
-
-	repoPath2 := tmpDir2
-	repo2, err := kapis_git.OpenRepository(repoPath2)
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-
-	if err := kapis_git.Checkout(repo2, "v0.0.2", "", nil); err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-
-	repo2Manifest, err := kuz(path.Join(repoPath2, "manifests", "docker-desktop"))
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-
-	if err := kapis_git.Checkout(repo1, "v0.0.2", "", nil); err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-
-	repo1Manifest, err := kuz(path.Join(repoPath1, "manifests", "docker-desktop"))
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-
-	if string(repo2Manifest) != string(repo1Manifest) {
-		t.Logf("manifest generated on the original config:\n%v", string(repo1Manifest))
-		t.Logf("manifest generated on the copied config:\n%v", string(repo2Manifest))
-		t.Fatal("expected manifests to be equal, but they were not")
+	if d, _ := directoryContentsEqual(repoPath1, tmpDir2); !d {
+		t.Log("Directory was not copied properly")
+		t.Fail()
 	}
 }
 
@@ -98,4 +81,35 @@ func kuz(directory string) ([]byte, error) {
 		return nil, err
 	}
 	return resMap.AsYaml()
+}
+
+func directoryContentsEqual(dir1 string, dir2 string) (bool, error) {
+	if map1, err := getDirMap(dir1); err != nil {
+		return false, err
+	} else if map2, err := getDirMap(dir2); err != nil {
+		return false, err
+	} else if !reflect.DeepEqual(map1, map2) {
+		return false, nil
+	}
+	return true, nil
+}
+
+func getDirMap(dir string) (map[string][]byte, error) {
+	dirMap := make(map[string][]byte)
+	if err := filepath.Walk(dir, func(fpath string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if fpath != dir && !info.IsDir() {
+			if fileContent, err := ioutil.ReadFile(fpath); err != nil {
+				return err
+			} else {
+				dirMap[path.Base(fpath)] = fileContent
+			}
+		}
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	return dirMap, nil
 }
